e59fa4af50a4b42a87cf68c05b353d8c2ac33460
Nayeem <s.nayeem@outlook.com>
2022-05-11 21:41:33 -0400

    moving turtle left and right

--- main.py
+++ main.py
@@ -10,12 +10,34 @@
 # TODO: Extra life
 
 from turtle import Screen
-
+from turtle import Turtle
 # screen setup
 game_screen = Screen()
 game_screen.title("BreakOut Game")
 game_screen.setup(width=800, height=800)
 game_screen.bgcolor("black")
 
+pad = Turtle()
+
+pad.shape("square")
+pad.color("white")
+pad.shapesize(stretch_wid=1, stretch_len=10)
+pad.penup()
+pad.goto(y=-350, x=0)
+pad.speed(0)
+
+def move_left():
+    pad.back(3)
+
+
+def move_right():
+    pad.forward(3)
+
+
+# start listening to key stocks
+
+game_screen.listen()
+game_screen.onkey(key="Left", fun=move_left)
+game_screen.onkey(key="Right", fun=move_right)
 
 game_screen.exitonclick()
